2f7fb4e700c7dbfbb3cc90a4a274dec3fb6025c8
Michele Mesiti <michele.mesiti@kit.edu>
2025-12-11 12:18:40 +0100

    scan-getXBubble-performance.sh improved for slurm

--- performance_engineering/scan-getXBubble-performance.sh
+++ performance_engineering/scan-getXBubble-performance.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --cpus-per-task=76
+#SBATCH --partition cpuonly
+#SBATCH --time 60
+#SBATCH --cpu-freq=3000000
 
 NTHREADS=76 # horeka
 
-for commit in $(git log --format=%h src/PMFRG_xyz.jl)
+for commit in $(git log --format=%h ../src/PMFRG_xyz.jl)
 do 
     git checkout $commit ../src/PMFRG_xyz.jl
-    COMMENT=$(git log -1 $commit --pretty-format="%cn %cr")
+    COMMENT="$(git log -1 $commit --format="%cn ; %s ; %ad")"
     julia --project=. --threads=$NTHREADS -O3 ./benchmark_and_test_getXBubble.jl --dbfile=getxbubble-scan.db --commit="$commit" --comment="$COMMENT"
 done
